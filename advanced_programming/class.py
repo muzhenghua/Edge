6d54a0810e45a8e596e8a0baabaa5a698ac613c5
mzx <muzx_sky@yeah.net>
2022-08-28 12:34:19 +0800

    python 学习项目fast_api

---
+++ advanced_programming/class.py
@@ -0,0 +1,92 @@
+import os
+
+
+class function:
+    _list=[]
+    def __init__(self, a):
+        self.a = a
+        pass
+
+    # 静态方法就是一个函数，可不用实例化就调用，无法调用类方法和类属性
+    @staticmethod
+    def get_info(a):
+        # print(self.a) # 报错无法使用
+        # self.__get_info_private()  # 报错无法使用
+
+        a = a + 'ss'
+        print('print')
+        return a
+
+    # 类方法，可以直接调用，不用实例化，也可实例化调用，无法调用类方法和类属性。
+    @classmethod
+    def get_info_class_method(cls):
+        # print(self.a) # 报错
+        cls.__get_info_private()# 会报错missing 1 required positional argument: 'self' 
+        print(cls.get_info('ss'))
+        print('class method')
+
+    # 通常针对同名函数的自定义操作
+    def _get_info_class_method(self):
+        print(self.a + 'class method' + '_')
+        return self.a + 'class method' + '_'
+
+    # 类实例方法，不接收参数，仅供内部调用。
+    def get_info_method(self, a):
+        print('method')
+        print(a)
+        self.__get_info_private()
+        return self.a + 'method'
+
+    # 内部函数,仅供内部调用。函数外部无法直接使用，但是可通过调用内部函数间接调用
+    def __get_info_private(self):
+        print(self.a+' private method')
+        return self.a+'private method'
+
+    # 类变量在类里面共享值，被修改后其他地方使用值也会被修改
+    def modify_v_a(self):
+        self.a += self.a
+
+    def _modify_v_a(self):
+        self.a += self.a
+
+
+# function.get_info_class_method()
+# function.get_info('ss')
+
+# func = function(a='ss')
+# # func.get_info_method('ss')
+# # func.get_info('ss')
+# # func.get_info_class_method()
+# # func._get_info_class_method()
+# func.modify_v_a()
+# func._modify_v_a()
+# print(func.a)
+# func1 = function(a='aa')
+# func._list.append('ssss')
+#
+# print(func._list)
+# print(func1._list)
+
+def get_pararmeter(*args, **kwargs):
+    print(args) # 不定长参数，他可以表示输入参数是不确定的，可以是任意多个。
+    print(kwargs) # 关键字参数，赋值的时候是以键= 值的方式
+
+get_pararmeter(['ss','ss'])
+
+a = 1
+c = [a]
+a = 2
+print(c)
+print(dir(a))
+ab = [lambda x: x*x [0,1,2]]
+print(ab)
+def multipliers1():
+    # 闭包,返回的四个函数， 
+    a = [lambda x: i * x for i in range(4)]
+    print(a)
+    print('=====')
+    return a
+a = multipliers1()
+for i in a:
+    print(i(10))
+# print([m(2) for m in a])
---
+++ fastapi_code/fastapi_db/db.py
@@ -0,0 +1,116 @@
+import sqlalchemy
+from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, String
+
+# 数据库类型
+from sqlalchemy.orm import Session
+
+DIALCT = "mysql"
+
+# 数据库驱动
+DRIVER = "pymysql"
+
+# 数据库用户名
+USERNAME = "root"
+
+# 数据库密码
+PASSWORD = "123456"
+
+# 服务器IP地址
+HOST = "127.0.0.1"
+
+# 端口号，默认3306
+PORT = "3306"
+
+# 数据库名
+DATABASE = "python"
+
+DB_URI = "{}+{}://{}:{}@{}:{}/{}"
+charset = DB_URI.format(DIALCT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
+
+print(charset)
+# 创建数据库引擎
+
+engine = create_engine(charset)
+
+
+def insert_sql(engine):
+    with engine.connect() as conn:
+        pass
+    #     conn.execute(text("CREATE TABLE IF NOT EXISTS some_table (x int, y int) "))
+    #     # 写入数据
+    #     conn.execute(text("INSERT INTO some_table (x, y) VALUES (:x, :y)"), [{"x": 1, "y": 1}, {"x": 2, "y": 4}])
+    #     conn.execute(text("INSERT INTO some_table (x, y) VALUES (:x, :y)"), [{"x": 11, "y": 12}, {"x": 13, "y": 14}])
+    #     # 更新数据
+    #     update_sql = text("UPDATE some_table SET y=:y WHERE x=:x")
+    #     update_date = [{"x": 11, "y": 11}, {"x": 13, "y": 13}]
+    #     conn.execute(update_sql, update_date)
+    #     print(type(engine))
+    pass
+
+
+def get_model():
+    with engine.connect() as conn:
+        # 执行基本的sql语法
+        result = conn.execute(text("SELECT x, y FROM some_table"))
+        # for row in result:
+        #     # sqlalchemy.engine.row.LegacyRow 对象，values 是一个元组，可以.column出对应的值
+        #     print(type(row))
+        #     print(dir(row))
+        #     print(row.y)
+        #     print(row)
+        for row in result.mappings():
+            # 转化为字典
+            print(type(row))
+            print(row)
+
+#
+# def sql_model(metadata_obj):
+#     user_table = Table(
+#         "user_account",
+#         metadata_obj,
+#         Column("id", Integer, primary_key=True),
+#         Column("name", String(30)),
+#         Column("fullname", String(20))
+#     )
+#     print(user_table.c.keys())
+
+
+def sql_model(metadata_obj):
+    user_table = Table(
+        "user_account",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("name", String(30)),
+        Column("fullname", String(20))
+    )
+    print(user_table.c.keys())
+
+
+def sql_model_update(metadata_obj):
+    user_table = Table(
+        "user_account",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("name", String(30)),
+        Column("fullname", String(20)),
+        Column("fullname_copy", String(20))
+    )
+    print(user_table.c.keys())
+
+
+def create_table_into_databse():
+    global metadata_obj
+    metadata_obj = MetaData()
+    sql_model(metadata_obj)
+    # create_all通过该对象传入数据库，创建表，如果不存在则创建，如果存在则不创建
+    metadata_obj.create_all(engine)
+    # drop_all 通过该对象传入数据库，删除表
+    metadata_obj.create_all(engine)
+
+
+create_table_into_databse()
+
+
+metadata_obj = MetaData()
+sql_model_update(metadata_obj)
+print(res)
