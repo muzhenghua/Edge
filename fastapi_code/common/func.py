f95f1f2f5bc6258bfa6abb08aa8aae50f939f464
mzx <muzx_sky@yeah.net>
2022-10-03 23:03:21 +0800

    sqlalchemy learn: 增加创建和插入sql函数，以及基础函数：字典值列表转化为列表值字典、函数运行时间装饰器。

--- fastapi_code/common/func.py
+++ fastapi_code/common/func.py
@@ -1,3 +1,5 @@
+import time
+
 from fastapi_code.common.db_session import database
 
 
@@ -10,3 +12,36 @@ def insert_database_by_dict(db_name: str, table_name: str, date: dict):
     """
     with database.session() as session:
         session.query().filter()
+
+
+def time_it(func):
+    """
+    :param func: 装饰器装饰函数
+    :return: 打印出函数执行时间，方便调优
+    """
+    def inner(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print('用时:{}秒'.format(end-start))
+    return inner
+
+
+def dict_value_list_to_list_value_dict(data: dict) -> list:
+    """
+    :param data: {"x":[1, 2]} or {"x":1]}
+    :return: [{"x":1},{"x":2}] or [{"x":1}]
+    """
+    date_list = []
+    tmp_values = list(data.values())
+    if isinstance(tmp_values[0], (str, int)):
+        date_list.append(data)
+
+    if isinstance(tmp_values[0], list):
+        for i in range(len(tmp_values[0])):
+            tmp_dict = {}
+            for key, v in data.items():
+                tmp_dict[key] = v[i]
+            date_list.append(tmp_dict)
+
+    return date_list
--- fastapi_code/db/sql.py
+++ fastapi_code/db/sql.py
@@ -1,5 +1,6 @@
 from sqlalchemy import text
 
+from fastapi_code.common.func import time_it, dict_value_list_to_list_value_dict
 from fastapi_code.seting.config import SqlDataType, SqlCreate
 from fastapi_code.common.db_session import database
 
@@ -7,23 +8,52 @@ from fastapi_code.common.db_session import database
 class SqlStatement:
 
     @staticmethod
+    @time_it
     def insert_sql(*args, **kwargs):
-        table_name = kwargs.get("table_name")
-        table_date = kwargs.get("table_date")
-        if not isinstance(table_date, dict):
-            raise TypeError(table_date)
+            """
+            :param args:
+            :param kwargs: 以字典的形式传入
+            表名“table_name”
+            数据“table_date”，eg:多行{"x":["1","2"]}，单行{"x":"1"},单行{"x":["1"]}
+            :return:
+            在示例中 提交更改 ，我们执行了一个INSERT语句，在该语句中我们似乎能够同时向数据库中插入多行。
+            对于那些 对数据进行操作，但不返回结果集 ，
+            即 DML 像“INSERT”这样的语句不包括“RETURNING”这样的短语，
+            我们可以发送 多参数 到 Connection.execute() 方法，
+            方法是传递字典列表而不是单个字典，从而允许针对每个参数集分别调用单个SQL语句：
+            """
+            table_name = kwargs.get("table_name")
+            table_data = kwargs.get("table_date")
+            if not table_data:
+                raise ValueError(table_data)
+            if not isinstance(table_data, dict):
+                raise TypeError(table_data)
+            col_list = table_data.keys()
 
-        col_list = []
-        col_date = [dict]
-        print(col_date)
-        col_list = table_date.keys()
-        for col, col_date in table_date.items():
-            col_list.append(":" + col)
+            data_list = dict_value_list_to_list_value_dict(table_data)
+            insert_table = f"INSERT INTO {table_name} ({', '.join(col_list)})" \
+                           f" VALUES ({', '.join(list(':' + i for i in col_list))})"
+            with database.get_engine().connect() as conn:
+                conn.execute(text(insert_table), data_list)
 
-        insert_table = f"INSERT INTO {table_name} () VALUE "
+    def merge_sql(self):
+        pass
+
+    def update_sql(self):
+        pass
+
+    def delete_sql(self):
+        pass
 
     @staticmethod
-    def create_sql_command(*args, **kwargs):
+    def create_sql(*args, **kwargs):
+        """
+        :param args:
+        :param kwargs: 以字典的形式传入
+            表名“table_name”
+            数据“table_date”，eg:{字段名:字段类型}
+        :return:
+        """
         table_name = kwargs.get("table_name")
         table_date: dict = kwargs.get("table_date")
         if not isinstance(table_date, dict):
@@ -36,12 +66,11 @@ class SqlStatement:
 
         create_table = f"CREATE TABLE {SqlCreate.IF_NOT_EXITS.value} {table_name} " \
                        f"({','.join(col_date_list)}) " \
-                       f"{SqlCreate.ENGINE.value}"
-        print(create_table)
+                       f"{SqlCreate.ENGINE.value}" 
         with database.get_engine().connect() as conn:
             conn.execute(text(create_table))
 
 
 a = ["s", "-ss"]
-print(":".join(a))
-# SqlStatement.insert_sql(table_name="mzx", table_date={"x": 'INT'})
+# print(":".join(a))
+SqlStatement.insert_sql(table_name="mzx", table_date={"x": 1, "y": 2})
